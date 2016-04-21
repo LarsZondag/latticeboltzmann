8f0019725f29e7877d8d75d755ace39f126b8681
LarsZondag <larszondag@gmail.com>
2016-04-21 17:03:39 +0200

    works pretty okay

--- code/python/main.py
+++ code/python/main.py
@@ -3,10 +3,10 @@ import numpy.linalg as linalg
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-maxIter = 1000
-Re = 10
-nx = 100
-ny = 50
+maxIter = 10000
+Re = 200
+nx = 500
+ny = 70
 q = 9
 r = ny / 9
 uLB = 0.1
@@ -26,10 +26,8 @@ rho = np.ones((nx, ny))
 f_i = np.zeros((nx, ny, q))
 uc = np.zeros((nx, ny, q))
 
-for i in range(nx):
-    for j in range(ny):
-        f_i[i, j, 0] = w_i[0] * rho[i, j]
 
+f_i[:, :, 0] = w_i[0] * rho
 f_eq = np.copy(f_i)
 
 
@@ -41,7 +39,7 @@ def equilibrium(rho, ux, uy):  # Equilibrium distribution function.
     u2 = ux ** 2 + uy ** 2
 
     for i in range(q):
-        feq[:,:,i] = rho * w_i[i] * (1 + uc / c_sqrd + (uc2 - c_sqrd * u2) / (2*c_sqrd **2))
+        feq[:,:,i] = rho * w_i[i] * (1 + uc[:,:,i] / c_sqrd + (uc2[:,:,i] - c_sqrd * u2) / (2*c_sqrd **2))
 
     return feq
 
@@ -70,7 +68,8 @@ for t in range(maxIter):
     for i in range(q):
         f_out[boundary, i] = f_out[boundary, opp[i]]
 
-    f_out[not_boundary,1] += 0.001
+    f_out[0,:,1] += uLB
+    # f_out[-1,:,1] -= uLB
 
     for i in range(q):
         f_i[:, :, i] = np.roll(np.roll(f_out[:, :, i], cx[i], axis=0), cy[i], axis=1)
@@ -82,28 +81,6 @@ for t in range(maxIter):
     uy = (np.sum(f_i[:, :, [2, 5, 6]], axis=2) - np.sum(f_i[:, :, [4, 7, 8]], axis=2)) / rho
 
 
-    u_sqrd = np.power(ux, 2) + np.power(uy, 2)
-    uxplusuy = ux + uy
-    minuxplusuy = -ux + uy
-    minuxminuy = -uxplusuy
-    plusuxminuy = -minuxplusuy
-
-    minuxplusuy2c2 = np.power(minuxplusuy, 2)
-    plusuxminuy2c2 = minuxplusuy2c2
-    uxplusuy2c2 = np.power(uxplusuy, 2)
-    minuxminuy2c2 = uxplusuy2c2
-    ux2c2 = np.power(ux / c_sqrd, 2)
-    uy2c2 = np.power(uy / c_sqrd, 2)
-
-    # f_eq[:, :, 0] = w_i[0] * rho * (1 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 1] = w_i[1] * rho * (1 + ux / c_sqrd + 0.5 * ux2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 2] = w_i[2] * rho * (1 + uy / c_sqrd + 0.5 * uy2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 3] = w_i[3] * rho * (1 - ux / c_sqrd + 0.5 * ux2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 4] = w_i[4] * rho * (1 - uy / c_sqrd + 0.5 * uy2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 5] = w_i[5] * rho * (1 + uxplusuy / c_sqrd + 0.5 * uxplusuy2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 6] = w_i[6] * rho * (1 + minuxplusuy / c_sqrd + 0.5 * minuxplusuy2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 7] = w_i[7] * rho * (1 + minuxminuy / c_sqrd + 0.5 * minuxminuy2c2 - u_sqrd / (2 * c_sqrd))
-    # f_eq[:, :, 8] = w_i[8] * rho * (1 + plusuxminuy / c_sqrd + 0.5 * plusuxminuy2c2 - u_sqrd / (2 * c_sqrd))
 
     f_eq = equilibrium(rho, ux, uy)
 
@@ -113,5 +90,5 @@ for t in range(maxIter):
 
     if (t % 50 == 0):
         plt.clf();
-        plt.imshow((ux ** 2 + uy ** 2).transpose(), cmap=cm.cool)
+        plt.imshow((np.sqrt(ux ** 2 + uy ** 2)).transpose(), cmap=cm.cool)
         plt.savefig("vel." + str(t / 100).zfill(4) + ".png")
