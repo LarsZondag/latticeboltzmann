d4e5273508ab6c7484836679da97c052ae410813
LarsZondag <larszondag@gmail.com>
2016-04-23 14:15:46 +0200

    Starting structuring

--- code/python/main.py
+++ code/python/main.py
@@ -16,34 +16,42 @@ obstacle_r = ny / 9
 nulb = uLB * obstacle_r / Re
 tau = (3. * nulb + 0.5)
 
-e_i = np.array([[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]])
-cx = np.array([0, 1, 0, -1, 0, 1, -1, -1, 1])
-cy = np.array([0, 0, 1, 0, -1, 1, 1, -1, -1])
-opp = np.array([0, 3, 4, 1, 2, 7, 8, 5, 6])
+# e_i gives the directions of all 9 velocity vectors.
+# opp gives the indices that correspond to the index of e_i containing the opposite velocity vector
+e_i = np.zeros((q, 2), dtype=np.int64)
+opp = np.zeros((q, 1), dtype=np.int8)
+e_i[0] = [ 0,  0];      opp[0] = 0
+e_i[1] = [ 1,  0];      opp[1] = 3
+e_i[2] = [ 0,  1];      opp[2] = 4
+e_i[3] = [-1,  0];      opp[3] = 1
+e_i[4] = [ 0, -1];      opp[4] = 2
+e_i[5] = [ 1,  1];      opp[5] = 7
+e_i[6] = [-1,  1];      opp[6] = 8
+e_i[7] = [-1, -1];      opp[7] = 5
+e_i[8] = [ 1, -1];      opp[8] = 6
+# e_i = np.array([[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [-1, -1], [1, -1]])
+# cx = np.array([0, 1, 0, -1, 0, 1, -1, -1, 1])
+# cy = np.array([0, 0, 1, 0, -1, 1, 1, -1, -1])
+# opp = np.array([0, 3, 4, 1, 2, 7, 8, 5, 6])
 w_i = np.array([4 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 9, 1 / 36, 1 / 36, 1 / 36, 1 / 36])
 
-c_sqrd = 2 / 3
 
-rho = np.ones((nx, ny))
-
-uc = np.zeros((nx, ny, q))
 
 
 @jit
-def equilibrium(rho, ux, uy):  # Equilibrium distribution function.
-    feq = np.zeros((nx, ny, q))
-    uc = np.zeros((nx, ny, q))
-    for i in range(q):
-        uc[:,:,i] = ux * cx[i] + uy * cy[i]
+def equilibrium(rho, max_x, max_y, directions, velo_x, velo_y, c):  # Equilibrium distribution function.
+    feq = np.zeros((max_x, max_y, directions))
+    uc = np.zeros((max_x, max_y, directions))
+    for i in range(directions):
+        uc[:,:,i] = velo_x * c[i, 0] + velo_y * c[i, 1]
     uc *= 3
     uc2 = uc ** 2
-    u2 = 3 / 2 * (ux ** 2 + uy ** 2)
+    u2 = 3 / 2 * (velo_x ** 2 + velo_y ** 2)
 
-    for i in range(q):
+    for i in range(directions):
         feq[:,:,i] = rho * w_i[i] * (1 + uc[:,:,i] + 0.5 * uc2[:,:,i] - u2)
 
     return feq
-
 
 def set_boundary(max_x, max_y, obst_x, obst_y, obst_r):
     b = np.zeros((max_x, max_y))
@@ -58,63 +66,79 @@ def poiseuille_flow_channel(max_x, max_y, u_max):
     uy = np.zeros((max_x, max_y))
     ux = np.zeros((max_x, max_y))
     for y in range(max_y):
-        ux[:,y] = (u_max / (ny /2 ) ** 2) * (1 - (np.abs(y-ny/2) / (ny / 2)) ** 2)
+        ux[:,y] = (u_max / (max_y /2 ) ** 2) * (1 - (np.abs(y-max_y/2) / (max_y / 2)) ** 2)
     return ux, uy
 
 def poiseulle_flow_boundary(max_y, u_max):
     ux = np.zeros((max_y))
     for y in range(max_y):
-        ux[y] = (u_max / (ny /2 ) ** 2) * (1 - (np.abs(y-ny/2) / (ny / 2)) ** 2)
+        ux[y] = (u_max / (max_y /2 ) ** 2) * (1 - (np.abs(y-max_y/2) / (max_y / 2)) ** 2)
     return ux
 
+# Initialize the boundary once. The boundary depends on the box's dimensions and the object being placed
 boundary = set_boundary(nx, ny, obstacle_x, obstacle_y, obstacle_r)
 not_boundary = boundary == False
 
-
+# Initialize the poiseulle flow velocity profile once to reference later
 p_flow_bdry = poiseulle_flow_boundary(ny, uLB)
 p_flow_channel_x, p_flow_channel_y = poiseuille_flow_channel(nx, ny, uLB)
 
+# Conditions to start the simulation with:
+# Starting velocity profile can be set here. We chose a poiseulle flow profile
 ux = p_flow_channel_x
 uy = p_flow_channel_y
+
+# Make sure there is not flow on the boundary
 ux[boundary] = 0
 uy[boundary] = 0
-f_i = equilibrium(rho, ux, uy)
+rho = np.ones((nx, ny))
+
+# The velocity profile is incorporated into the equilibrium distribution.
+f_i = equilibrium(rho, nx, ny, q, ux, uy, e_i)
 f_eq = np.copy(f_i)
-f_out = np.copy(f_i)
 
 for t in range(maxIter):
+    # Right side boundary condition to make sure the flow doesn't go backwards
     f_i[ -1, :, [3, 6, 7]] = f_i[ -2, :, [3, 6, 7]]
+
+    # Calculate density and velocities
     rho = np.sum(f_i, axis=2)
     ux = (np.sum(f_i[:, :, [1, 5, 8]], axis=2) - np.sum(f_i[:, :, [3, 6, 7]], axis=2)) / rho
     uy = (np.sum(f_i[:, :, [2, 5, 6]], axis=2) - np.sum(f_i[:, :, [4, 7, 8]], axis=2)) / rho
 
+    # Increment velocities in x-direction to mimic a constant pressure drop
     ux += 0.7 * p_flow_channel_x
-    # rho[0, :] = 1. / (1. - ux[0]) * (np.sum(f_i[0, :, [0, 2, 4]], axis=0) + 2. * np.sum(f_i[0, :, [3, 6, 7]], axis=0))
-    f_eq = equilibrium(rho, ux, uy)
 
+    # Calculate the equilibrium distribution based on the density and velocities
+    f_eq = equilibrium(rho, nx, ny, q, ux, uy, e_i)
+
+    # Collision / relaxation step
     f_out = f_i - (f_i - f_eq) / tau
 
+    # Bounce-back boundary conditions. This step flips the directions of the velocities of all points lying inside objects.
     for i in range(q):
         f_out[boundary, i] = f_i[boundary, opp[i]]
 
+    # Streaming step
     for i in range(q):
-        f_i[:, :, i] = np.roll(np.roll(f_out[:, :, i], cx[i], axis=0), cy[i], axis=1)
+        f_i[:, :, i] = np.roll(np.roll(f_out[:, :, i], e_i[i, 0], axis=0), e_i[i, 1], axis=1)
 
+    # Make images of velocity in the x and y direction and of the speed
     if (t % 100 == 0):
         plt.clf();
-        plt.imshow((ux).transpose(), cmap=cm.Blues)
-        # plt.clim(0,uLB)
+        plt.imshow(ux.transpose(), cmap=cm.Blues)
         plt.colorbar()
-        plt.savefig("velx/" + str(t / 100).zfill(4) + ".png")
+        plt.savefig("velx/" + str(t / 100) + ".png")
+
         plt.clf();
-        plt.imshow((uy).transpose(), cmap=cm.Blues)
+        plt.imshow(uy.transpose(), cmap=cm.Blues)
         plt.colorbar()
-        plt.savefig("vely/" + str(t / 100).zfill(4) + ".png")
+        plt.savefig("vely/" + str(t / 100) + ".png")
+
         plt.clf();
         plt.imshow((np.sqrt(ux ** 2 + uy ** 2)).transpose(), cmap=cm.Blues)
-        # plt.clim(0,uLB)
         plt.colorbar()
-        plt.savefig("vel/" + str(t / 100).zfill(4) + ".png")
+        plt.savefig("vel/" + str(t / 100) + ".png")
 
 
 
